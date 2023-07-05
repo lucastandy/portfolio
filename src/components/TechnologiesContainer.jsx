ccee74ce03d23cd7a99cc8a4de36033edafbfa9c
lucastandy <lucastitandy@gmail.com>
2023-07-05 11:52:18 -0300

    Atualizando a lista de stacks e estilos

--- src/components/TechnologiesContainer.jsx
+++ src/components/TechnologiesContainer.jsx
@@ -1,12 +1,12 @@
-import {DiHtml5, DiCss3, DiJsBadge, DiNodejsSmall, DiMysql, DiReact} from "react-icons/di";
+import {DiPhp, DiPython, DiJsBadge, DiNodejsSmall, DiMysql, DiPostgresql} from "react-icons/di";
 import '../styles/components/technologiescontainer.sass';
 const technologies = [
-    { id: "html", name: "HTML5", icon: <DiHtml5 /> },
-    { id: "css", name: "CSS3", icon: <DiCss3 /> },
-    { id: "js", name: "JavaScript", icon: <DiJsBadge /> },
-    { id: "node", name: "Node.js", icon: <DiNodejsSmall /> },
-    { id: "mysql", name: "MySQL", icon: <DiMysql /> },
-    { id: "react", name: "React", icon: <DiReact /> },
+    { id: "php", name: "PHP", text:"Ampla Experiência em desenvolvimento com PHP",icon: <DiPhp /> },
+    { id: "python", name: "Python", text:"Experiência em análise de dados com Python", icon: <DiPython /> },
+    { id: "js", name: "JavaScript", text:"Experiência em desenvolvimento com JavaScript", icon: <DiJsBadge /> },
+    { id: "node", name: "Node.js", text:"Experiência em backend com Node", icon: <DiNodejsSmall /> },
+    { id: "mysql", name: "MySQL", text:"Experiência com o banco de dados MySql", icon: <DiMysql /> },
+    { id: "postgres", name: "PostgreSql", text:"Experiência com o banco de dados PostgreSQL", icon: <DiPostgresql /> },
   ];
 const TechnologiesContainer = () => {
     return <section className="technologies-container">
@@ -17,7 +17,7 @@ const TechnologiesContainer = () => {
           {tech.icon}
           <div className="technology-info">
             <h3>{tech.name}</h3>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            <p>{tech.text}</p>
           </div>
         </div>
       ))}
