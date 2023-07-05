b3081bc4518ee862ca3688a7af955eed5fde4178
lucastandy <lucastitandy@gmail.com>
2023-07-05 11:51:07 -0300

    Atualização da seção projetos

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -1,8 +1,8 @@
 const ProjectsContainer = () => {
     return <section className="projects-container">
         <h2>Projetos</h2>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis accusantium, molestiae dolorum praesentium cum iusto, porro nisi modi facilis nobis recusandae ratione exercitationem quidem eaque, animi nostrum beatae accusamus laudantium?</p>
-        <a href="" className="btn">
+        <p>Confira alguns dos meus projetos no GitHub</p>
+        <a href="https://github.com/lucastandy" className="btn" target="_blank">
             Ver Projetos
         </a>
     </section>
