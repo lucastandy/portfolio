8223d265acf7f16d19a7b025fa35872fd4684148
lucastandy <lucastitandy@gmail.com>
2023-07-05 11:51:31 -0300

    atualizando os links das redes sociais

--- src/components/SocialNetwors.jsx
+++ src/components/SocialNetwors.jsx
@@ -3,15 +3,15 @@ import {FaLinkedinIn, FaGithub, FaInstagram} from 'react-icons/fa';
 import '../styles/components/socialnetworks.sass';
 
 const socialNetworks = [
-    {name: "linkedin", icon: <FaLinkedinIn />},
-    {name: "github", icon: <FaGithub />},
-    {name: "instagram", icon: <FaInstagram />}
+    {name: "linkedin", link:"https://www.linkedin.com/in/lucas-tandy/", icon: <FaLinkedinIn />},
+    {name: "github", link:"https://github.com/lucastandy", icon: <FaGithub />},
+    {name: "instagram", link:"https://www.instagram.com/lucastandy/",  icon: <FaInstagram />}
 ];
 
 const SocialNetworks = () => {
     return (<section id="social-networks">
         {socialNetworks.map((network) => (
-            <a href="#" className='social-btn' id={network.name} key={network.name}>
+            <a href={network.link} target='_blank' className='social-btn' id={network.name} key={network.name}>
                 {network.icon}
             </a>
         ))}
