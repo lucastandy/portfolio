a1b795138b4cd2de99254124dcf4da6c310bd895
lucastandy <lucastitandy@gmail.com>
2023-10-13 14:32:19 -0300

    alteração no sobre

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -1,7 +1,7 @@
 const AboutContainer = () => {
     return <section className="about-container">
         <h2>Sobre</h2>
-        <p>Pós-graduando em Engenharia de Software pela Estácio, formado em Ciências da Computação pela Universidade Estadual Vale do Acaraú - UVA, tem experiência com as stacks: PHP, JavaScript, Python, Node, MySql, PostreSQL e demais ferramentas.</p>
+        <p>Pós-graduado em Engenharia de Software pela Estácio, formado em Ciências da Computação pela Universidade Estadual Vale do Acaraú - UVA, tem experiência com as stacks: PHP, JavaScript, Python, Node, MySql, PostreSQL e demais ferramentas.</p>
         <p>Fique à vontade para entrar em contato comigo para explorarmos maneiras de trabalhar juntos e impulsionar o sucesso do seu negócio!</p>
     </section>
 };
